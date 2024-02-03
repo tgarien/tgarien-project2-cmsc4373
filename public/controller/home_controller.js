afd2abfefd7b4728571cf43e7d9a60756bb8de77
Tad Garien <tadgarien@gmail.com>
2024-02-03 14:08:14 -0600

    final commit

---
+++ public/controller/home_controller.js
@@ -0,0 +1,13 @@
+export function onSubmitCalcForm(e){
+    e.preventDefault();
+    const nStr = e.target.number.value;
+    const n = parseInt(nStr);
+    const ul = document.getElementById('display');
+    const eqString = `
+        ${n} x ${n} = ${n*n}
+    `;
+    const li = document.createElement('li');
+    li.textContent = eqString;
+    ul.appendChild(li);
+    e.target.number.value = ''; // clear input field
+}
--- public/view/home_page.js
+++ public/view/home_page.js
@@ -1,11 +1,23 @@
 import { currentUser } from "../controller/firebase_auth.js";
 import { root } from "./elements.js";
 import { protectedView } from "./protected_view.js";
+import { onSubmitCalcForm } from "../controller/home_controller.js";
 
 export async function homePageView(){
     if(!currentUser){
         root.innerHTML = await protectedView();
         return;
     }
-    root.innerHTML = '<h1>Home Page</h1>';
+
+    const response = await fetch('/view/templates/home_page_template.html',
+        {cache: 'no-store'});
+    const divWrapper = document.createElement('div');
+    divWrapper.innerHTML = await response.text();
+    divWrapper.classList.add('m-4','p-4')
+
+    const form = divWrapper.querySelector('form');
+    form.onsubmit = onSubmitCalcForm;
+
+    root.innerHTML = '';
+    root.appendChild(divWrapper);
 }
